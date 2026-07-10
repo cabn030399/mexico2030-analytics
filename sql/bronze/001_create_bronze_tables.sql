afadc97dcf68c362e59f26202763e5ec82e34923
CARLOS BORJA <armandoborjanava@gmail.com>
2026-07-10 13:57:51 -0700

    Update 001_create_bronze_tables.sql

--- sql/bronze/001_create_bronze_tables.sql
+++ sql/bronze/001_create_bronze_tables.sql
@@ -1,1 +1,25 @@
+-- =====================================================
+-- Proyecto: Mexico2030 Analytics
+-- Script: 001_create_bronze_tables.sql
+-- =====================================================
 
+CREATE TABLE `mexico2030analytics.bronze.raw_matches`
+(
+    ingestion_timestamp TIMESTAMP,
+    source STRING,
+    payload JSON
+);
+
+CREATE TABLE `mexico2030analytics.bronze.raw_rankings`
+(
+    ingestion_timestamp TIMESTAMP,
+    source STRING,
+    payload JSON
+);
+
+CREATE TABLE `mexico2030analytics.bronze.raw_players`
+(
+    ingestion_timestamp TIMESTAMP,
+    source STRING,
+    payload JSON
+);
